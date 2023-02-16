1718725bc43fdc7b1944babcc3d28be0624f4631
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-02-16 20:55:03 +0200

    Update and rename README.md to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stos {
+public:
+    Stos() { }
+
+    void push(int Symbol) {
+        Symbols.push_back(Symbol);
+    }
+
+    void pop() {
+        Symbols.pop_back();
+    }
+
+    void ShowStos() {
+        cout << top() << "\n";
+    }
+
+    int top() {
+        return Symbols[getSize()];
+    }
+
+    int getSize() {
+        return Symbols.size() - 1;
+    }
+
+private:
+    vector<int> Symbols;
+};
+
+int main() {
+    Stos stos;
+    int i_temp;
+    int in_size, out_size;
+    cout << "How many values should be recorded: "; cin >> in_size;
+    cout << "How many elements to output: "; cin >> out_size;
+
+    for (int i = 0; i < in_size; ++i) {
+        cout << "Write symbol: "; cin >> i_temp; stos.push(i_temp);
+    }
+
+    for (int i = 0; i < out_size; ++i) {
+        if (stos.getSize() == -1) {
+            cout << "\nStos - empty";
+            break;
+        }
+
+        cout << "\nSymbol: " << stos.top(); stos.pop();
+    }
+
+    return 0;
+}
+
+/*
+Напишіть простий клас, який містить якісь елементи. Стос — це проста структура даних з
+двома операціями: push, яка додає елемент до колекції, і pop, яка вилучає останній
+доданий елемент.
+
+Ці два методи (push і pop) змінюють вміст стоса.
+
+Окрім цих методів, додайте метод, щоб отримати доступ до верху без модифікації стека
+(назвіть його top).
+
+Якщо стос порожній, додайте якийсь виняток. Вашим основним
+
+завданням є написання функцій оператора отримання значення зі стоса без його
+вилучення. До основного коду додайте свій код, щоб обробляти ці конкретні
+винятки. Крім того, в основному коді, додати трохи коду для введення даних.
+
+Запитайте користувача, скільки значень треба записати, скільки елементів ваша
+програма має вивести. Потім роздрукуйте стос — якщо виникне виняток,
+надрукуйте адекватне повідомлення. Використовуйте оператори, визначені у вашій
+програмі.
+
+Приклад введення
+1
+2
+3
+4
+5
+6
+
+Приклад виводу
+6
+5
+4
+3
+2
+1
+
+Виняток: стос порожній.
+*/
